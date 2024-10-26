fb1ca3cbd7d9ca6c86f60f74fbf9b445975cc89d
chinmaynawkar <cnawkar19@gmail.com>
2024-10-26 10:30:27 +0530

    feat:created startup card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 
@@ -5,6 +6,27 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
   //searchParams is a promise that resolves to an object with a query property
   const query = (await searchParams).query;
 
+  const posts = [{ 
+    _createdAt: new Date().toISOString(),
+    views: 100,
+    _id: '123',
+    author: { _id: 1, name: 'Chinmay'},
+    description: 'CyberTech Platform is a platform for startups to pitch their ideas to investors.',
+    image: 'https://picsum.photos/100/100',
+    category: 'Technology',
+    title: 'CyberTech Platform',
+  },
+  {
+    _createdAt: new Date().toISOString(),
+    views: 250,
+    _id: '125',
+    author: { _id: 2, name: 'Sarah'},
+    description: 'EcoGrow is an innovative app that helps urban dwellers create and maintain sustainable home gardens.',
+    image: 'https://picsum.photos/300/400',
+    category: 'Sustainability',
+    title: 'EcoGrow: Urban Gardening Made Easy',
+  }
+]
   return (
     <>
       <section className="pink_container">
@@ -17,6 +39,23 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
           investors.
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className='section_container'>
+        <p className="text-3xl font-semibold">
+          {query ? `Search results for "${query}"` : 'Latest startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {/* TODO: Fetch startups from database */}
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-result">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
